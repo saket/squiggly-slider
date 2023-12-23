b699eb97d7126b7532dd55790e0c4f66b778b8b9
Saket Narayan <saket@saket.me>
2023-12-23 13:38:31 -0500

    Add a sample

--- sample/src/main/kotlin/me/saket/squiggles/sample/SampleActivity.kt
+++ sample/src/main/kotlin/me/saket/squiggles/sample/SampleActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -11,11 +14,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.compose.ui.unit.dp
+import me.saket.squiggles.SquigglySlider
 
 @OptIn(ExperimentalMaterial3Api::class)
 class SampleActivity : AppCompatActivity() {
@@ -23,23 +30,28 @@ class SampleActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
 
     setContent {
-      val uiController = rememberSystemUiController()
-      val systemInDarkTheme = isSystemInDarkTheme()
-      LaunchedEffect(Unit) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        uiController.setSystemBarsColor(Color.Transparent, darkIcons = !systemInDarkTheme)
-        uiController.setNavigationBarColor(Color.Transparent)
-      }
-
-      val colors = if (systemInDarkTheme) dynamicDarkColorScheme(this) else dynamicLightColorScheme(this)
-
+      val colors = if (isSystemInDarkTheme()) dynamicDarkColorScheme(this) else dynamicLightColorScheme(this)
       MaterialTheme(colors) {
         Scaffold(
           topBar = {
             TopAppBar(title = { Text(stringResource(R.string.app_name)) })
           }
         ) { contentPadding ->
-          TODO()
+          Box(
+            modifier = Modifier
+              .fillMaxSize()
+              .padding(contentPadding)
+              .padding(24.dp),
+            contentAlignment = Alignment.Center,
+          ) {
+            var sliderValue by remember {
+              mutableStateOf(0.4f)
+            }
+            SquigglySlider(
+              value = sliderValue,
+              onValueChange = { sliderValue = it },
+            )
+          }
         }
       }
     }
